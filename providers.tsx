125e37667c61f28f0b9efd0a60ccbc899d200229
isyraqsadli <isyraq.sajid@broom.id>
2025-12-25 22:24:58 +0700

    init

---
+++ providers.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface ProvidersProps {
+  children?: React.ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+const Providers: React.FC<ProvidersProps> = (props) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default Providers;
